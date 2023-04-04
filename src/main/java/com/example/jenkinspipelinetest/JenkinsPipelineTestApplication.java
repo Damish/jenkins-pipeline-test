d6089064ace68ef00495a2a32e474c6747b82d6f
Damish <damishs88@gmail.com>
2023-04-04 17:06:59 +0530

    initial commit

---
+++ src/main/java/com/example/jenkinspipelinetest/JenkinsPipelineTestApplication.java
@@ -0,0 +1,23 @@
+package com.example.jenkinspipelinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsPipelineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineTestApplication.class, args);
+
+		System.out.println("Application started!!!");
+	}
+
+	@GetMapping(value = "/")
+	public String test(){
+		return "Hello world!";
+	}
+
+}
---
+++ src/test/java/com/example/jenkinspipelinetest/JenkinsPipelineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspipelinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
